2ee3edf28836903dc6d2b9a8284c8a086569c7e2
domix24 <domix24@users.noreply.github.com>
2025-08-07 23:49:47 -0400

    y19d15

---
+++ lib/year2019/day15.ex
@@ -0,0 +1,309 @@
+defmodule Aletopelta.Year2019.Day15 do
+  @moduledoc """
+  Day 15 of Year 2019
+  """
+  defmodule Common do
+    @moduledoc """
+    Common part for Day 15
+    """
+
+    @type input() :: list(binary())
+    @type intcode() :: %{integer() => integer()}
+    @type point() :: {integer(), integer()}
+    @type grid() :: %{point() => integer()}
+
+    @spec parse_input(input()) :: intcode()
+    def parse_input(input) do
+      input
+      |> Enum.at(0)
+      |> String.split(",")
+      |> Enum.with_index(fn string, index -> {index, String.to_integer(string)} end)
+      |> Map.new()
+    end
+
+    defp do_reduce(map, i, options, base),
+      do:
+        map
+        |> Map.fetch!(i)
+        |> parse_operator()
+        |> clean_options(options)
+        |> follow_command(map, i, base)
+        |> update_parameters()
+
+    defp clean_options(operator, options), do: {operator, Map.drop(options, [:state])}
+
+    defp follow_command({operator, options}, map, i, base),
+      do: get_command(operator, map, i, options, base)
+
+    defp parse_operator(operator) do
+      [smode3, smode2, smode1 | soperator] =
+        "#{operator}"
+        |> String.pad_leading(5, "0")
+        |> String.graphemes()
+
+      noperator =
+        soperator
+        |> Enum.join("")
+        |> String.to_integer()
+
+      {noperator, [smode1, smode2, smode3]}
+    end
+
+    defp update_parameters({map, i, %{state: state} = opt, base}) when state in [:stop, :pause],
+      do: {map, i, opt, base}
+
+    defp update_parameters({map, i, opt, base}), do: do_reduce(map, i, opt, base)
+
+    defp get_command({99, _}, map, i, %{output: value} = opt, base) do
+      new_map = Map.put(map, 0, value)
+      new_opt = Map.put(opt, :state, :stop)
+
+      {new_map, i, new_opt, base}
+    end
+
+    defp get_command({command, [mode1, mode2, mode3]}, map, i, opt, base)
+         when command in [1, 2, 7, 8] do
+      first_value = get_value(map, i + 1, mode1, base)
+      second_value = get_value(map, i + 2, mode2, base)
+
+      result = do_operation(command, first_value, second_value)
+      new_map = set_value(map, i + 3, mode3, base, result)
+
+      {new_map, i + 4, opt, base}
+    end
+
+    defp get_command({3, _}, map, i, %{input: []} = opt, base) do
+      new_opt = Map.put(opt, :state, :pause)
+
+      {map, i, new_opt, base}
+    end
+
+    defp get_command({3, [mode1 | _]}, map, i, %{input: [value | rest]} = opt, base) do
+      new_map = set_value(map, i + 1, mode1, base, value)
+
+      new_opt = Map.put(opt, :input, rest)
+
+      {new_map, i + 2, new_opt, base}
+    end
+
+    defp get_command({4, [mode1, _, _]}, map, i, opt, base) do
+      first_value = get_value(map, i + 1, mode1, base)
+
+      new_opt = Map.update(opt, :output, [first_value], &[first_value | &1])
+
+      {map, i + 2, new_opt, base}
+    end
+
+    defp get_command({command, [mode1, mode2, _]}, map, i, opt, base) when command in 5..6 do
+      first_value = get_value(map, i + 1, mode1, base)
+      second_value = get_value(map, i + 2, mode2, base)
+
+      if jump?(command, first_value) do
+        {map, second_value, opt, base}
+      else
+        {map, i + 3, opt, base}
+      end
+    end
+
+    defp get_command({9, [mode1 | _]}, map, i, opt, base) do
+      first_value = get_value(map, i + 1, mode1, base)
+
+      {map, i + 2, opt, base + first_value}
+    end
+
+    defp do_operation(1, param1, param2), do: param1 + param2
+    defp do_operation(2, param1, param2), do: param1 * param2
+
+    defp do_operation(7, param1, param2) when param1 < param2, do: 1
+    defp do_operation(7, _, _), do: 0
+
+    defp do_operation(8, param1, param1), do: 1
+    defp do_operation(8, _, _), do: 0
+
+    defp jump?(5, param), do: param != 0
+    defp jump?(6, param), do: param == 0
+
+    defp get_value(map, i, "0", _) do
+      position = Map.get(map, i, 0)
+      Map.get(map, position, 0)
+    end
+
+    defp get_value(map, i, "1", _) do
+      Map.get(map, i, 0)
+    end
+
+    defp get_value(map, i, "2", base) do
+      position = Map.get(map, i, 0)
+      Map.get(map, position + base, 0)
+    end
+
+    defp set_value(map, i, "0", _, value) do
+      position = Map.get(map, i, 0)
+      Map.put(map, position, value)
+    end
+
+    defp set_value(map, i, "2", base, value) do
+      position = Map.get(map, i, 0)
+      Map.put(map, position + base, value)
+    end
+
+    @spec prepare(intcode(), [integer()], integer(), integer()) ::
+            {%{program: intcode(), index: integer(), base: integer()}, [integer()],
+             :stop | :pause}
+    def prepare(map, input, index, base),
+      do:
+        map
+        |> do_reduce(index, %{input: input}, base)
+        |> format_output()
+
+    defp format_output({program, index, %{output: output, state: state}, base}),
+      do: {%{program: program, index: index, base: base}, output, state}
+
+    @spec build_grid(intcode()) :: grid()
+    def build_grid(intcode) do
+      position = {0, 0}
+      grid = Map.new([{position, 1}])
+      memory = Map.new(program: intcode, index: 0, base: 0)
+      direction = 1
+
+      memory
+      |> do_grid(direction, grid, position)
+      |> Enum.filter(fn {_, obj} -> obj > 0 end)
+      |> Map.new()
+    end
+
+    defp do_grid(memory, direction, grid, position) do
+      {new_memory, [object], _} =
+        Common.prepare(memory.program, [direction], memory.index, memory.base)
+
+      {new_grid, new_position, new_direction} = dispatch(object, grid, position, direction)
+
+      if new_position == {0, 0} do
+        new_grid
+      else
+        do_grid(new_memory, new_direction, new_grid, new_position)
+      end
+    end
+
+    defp clockwise(1), do: 3
+    defp clockwise(3), do: 2
+    defp clockwise(2), do: 4
+    defp clockwise(4), do: 1
+
+    defp counterclockwise(1), do: 4
+    defp counterclockwise(3), do: 1
+    defp counterclockwise(2), do: 3
+    defp counterclockwise(4), do: 2
+
+    defp update_position({x, y}, 1), do: {x, y + 1}
+    defp update_position({x, y}, 2), do: {x, y - 1}
+    defp update_position({x, y}, 3), do: {x + 1, y}
+    defp update_position({x, y}, 4), do: {x - 1, y}
+
+    defp dispatch(0, grid, position, direction) do
+      wall_position = update_position(position, direction)
+      new_grid = Map.put(grid, wall_position, 0)
+
+      new_direction =
+        Enum.reduce_while(0..2, direction, fn _, acc_direction ->
+          new_direction = clockwise(acc_direction)
+          new_position = update_position(position, new_direction)
+
+          if Map.get(new_grid, new_position) == 0 do
+            {:cont, new_direction}
+          else
+            {:halt, new_direction}
+          end
+        end)
+
+      {new_grid, position, new_direction}
+    end
+
+    defp dispatch(object, grid, position, direction) when object in [1, 2] do
+      new_position = update_position(position, direction)
+      new_direction = counterclockwise(direction)
+      new_grid = Map.put(grid, new_position, object)
+
+      {new_grid, new_position, new_direction}
+    end
+
+    @spec get_nexts(point(), grid(), %{point() => list(point())}) :: list(point())
+    def get_nexts({x, y}, grid, graph) do
+      [{-1, 0}, {1, 0}, {0, -1}, {0, 1}]
+      |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
+      |> Enum.filter(fn point -> Map.get(grid, point, 0) > 0 and Map.get(graph, point) == nil end)
+    end
+  end
+
+  defmodule Part1 do
+    @moduledoc """
+    Part 1 of Day 15
+    """
+    @spec execute(Common.input(), []) :: integer()
+    def execute(input, []) do
+      input
+      |> Common.parse_input()
+      |> Common.build_grid()
+      |> build_graph()
+    end
+
+    defp build_graph(grid) do
+      {0, 0}
+      |> build_graph(Map.new(), 0, grid)
+      |> elem(1)
+    end
+
+    defp build_graph(point, graph, depth, grid) do
+      nexts = Common.get_nexts(point, graph, grid)
+
+      fulldepth = if Map.get(grid, point) == 2, do: depth, else: nil
+
+      new_graph = Map.put(graph, point, nexts)
+
+      Enum.reduce(nexts, {new_graph, fulldepth}, fn nextpts, {acc_graph, acc_depth} ->
+        {new_graph, old_depth} = build_graph(nextpts, acc_graph, depth + 1, grid)
+
+        if old_depth do
+          {new_graph, old_depth}
+        else
+          {new_graph, acc_depth}
+        end
+      end)
+    end
+  end
+
+  defmodule Part2 do
+    @moduledoc """
+    Part 2 of Day 15
+    """
+    @spec execute(Common.input(), []) :: integer()
+    def execute(input, []) do
+      input
+      |> Common.parse_input()
+      |> Common.build_grid()
+      |> build_graph()
+    end
+
+    defp build_graph(grid) do
+      grid
+      |> Enum.find_value(&find_target/1)
+      |> build_graph(Map.new(), 0, grid)
+      |> elem(1)
+    end
+
+    defp find_target({point, 2}), do: point
+    defp find_target(_), do: nil
+
+    defp build_graph(point, graph, depth, grid) do
+      nexts = Common.get_nexts(point, graph, grid)
+
+      new_graph = Map.put(graph, point, nexts)
+
+      Enum.reduce(nexts, {new_graph, depth}, fn nextpts, {acc_graph, acc_depth} ->
+        {new_graph, old_depth} = build_graph(nextpts, acc_graph, depth + 1, grid)
+
+        {new_graph, max(acc_depth, old_depth)}
+      end)
+    end
+  end
+end
