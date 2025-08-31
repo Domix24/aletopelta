76f56684dd785e0946c369fb94465100b86824de
domix24 <domix24@users.noreply.github.com>
2025-08-30 23:44:29 -0400

    y19d19

---
+++ test/year2019/day19_test.exs
@@ -0,0 +1,13 @@
+defmodule AletopeltaTest.Year2019.Day19 do
+  use ExUnit.Case, async: true
+  alias Aletopelta.Year2019.Day19, as: Solution
+
+  @tag timeout: :infinity
+  test "is part1 working" do
+    assert Belodon.solve(Solution, :part1) == 154
+  end
+
+  test "is part2 working" do
+    assert Belodon.solve(Solution, :part2) == 9_791_328
+  end
+end
